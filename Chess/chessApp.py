68aa1803f67151faaa5ba5e93f3cf3c8d16f4d03
Mason Fox <mgfox@clemson.edu>
2020-05-29 14:49:50 -0400

    values for pieces

--- Chess/chessApp.py
+++ Chess/chessApp.py
@@ -1,26 +1,49 @@
 import chess
 import random
 
-# TODO : Make an output file that holds transcript of moves by each player
-# TODO : Structure code for python
-# TODO : Values for pieces
 # TODO : min/max tree
 
+#piece = piece object containing piece type and color
+#returns value of piece
+def piece_val(piece):
+    #White pos, Black neg
+    if(piece.color):
+        colorVal = 1
+    else:
+        colorVal = -1
 
-def compRandMove(board, notationFile):
+    if(piece.piece_type is 1):  
+        return 1 * colorVal
+    elif(piece.piece_type is 2 or piece.piece_type is 3):
+        return 3 * colorVal
+    elif(piece.piece_type is 4):
+        return 5 * colorVal
+    elif(piece.piece_type is 5):
+        return 9 * colorVal
+    else:
+        return 10000 * colorVal
+
+#Chooses a random move from list of legal moves, writes choice to output file and executes move
+#board = current state of board
+#notationFile = file to direct output to
+def rand_move(board, notationFile):
     #make random move from list of legal moves
     move = random.choice(list(board.legal_moves))
+    #add to notation file
     if(board.turn is chess.WHITE):
         outString = board.san(move) + "\t"
     else:
         outString = board.san(move) + "\n"
     notationFile.write(outString)
+
     board.push_uci(str(move))
     
-    
-def getMove(board, notationFile):
+#Prompts the user to enter move until valid move is chosen, writes move to output file and exectutes move on board
+#board = current state of board
+#notationFile = file to direct output to
+def player_move(board, notationFile):
 
-     #Get player move and validate
+    #Get player move and validate
     notValid = True
     sanMove = input("Enter move: ")
     while(notValid):
@@ -37,20 +60,27 @@ def getMove(board, notationFile):
             print("Not a valid move!")
             sanMove = input("Enter move: ")
 
+    #add move to notation file
     if(board.turn is chess.WHITE):
         outString = sanMove + "\t"
     else:
         outString = sanMove + "\n"
     notationFile.write(outString)
-    return uciMove
 
-def newGame():
+    board.push_uci(str(uciMove))
+
+#Creates a new board and opens output file
+#prompt user for white or black, loops moves until game ends
+def new_game():
    
+    #create board and notation file
     board = chess.Board()
+
+    test = chess.Piece(4, True)
+    print(piece_val(test))
 
     notationFile = open("notation.txt", "w+")
     notationFile.write("Wite\tBlack\n-------------\n");
-    
 
     #Prompt for color
     choice = input("W for White, B for Black:")
@@ -59,17 +89,16 @@ def newGame():
 
     #If black, computer makes first move
     if choice is "B":
-        compRandMove(board, notationFile)
-        
-        
+        rand_move(board, notationFile)
+          
     print()
     print(board, "\n")
     #Continue moves until game ends
     while not board.is_game_over():
         
         #Move player and computer
-        board.push_uci(str(getMove(board, notationFile)))
-        compRandMove(board, notationFile)
+        player_move(board, notationFile)
+        rand_move(board, notationFile)
         print(board, "\n")
 
     #Display reason for end of game
@@ -82,4 +111,4 @@ def newGame():
     else:
         print("Game Over")
 
-newGame()
+new_game()
